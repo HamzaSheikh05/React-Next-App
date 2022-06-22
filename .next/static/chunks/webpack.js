42c29501c512e76d28e2fb08baa5a16d34803e3c
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-22 21:38:43 +0400

    Divided the code into seperate components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "00e278ea4d575955"; }
+/******/ 		__webpack_require__.h = function() { return "be72db5d3b4e5bc7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
