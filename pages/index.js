2e489e8e91c174c02a44bbc5527e99d106b1d4ef
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-22 19:35:09 +0400

    import data from SpeakerData and destructure

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import {data} from "../SpeakerData";
+
 const IndexPage = () => {
+    const{id, bio, first, last, favorite, twitterHandle, company, sessions,} = data[0];
     return(
         <div>Hello from pluralsight!</div>
     )
