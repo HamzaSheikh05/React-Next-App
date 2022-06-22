342dd0c2c5dc3e8f06d3772dfd9936600afc205d
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-22 22:26:35 +0400

    Add onChange handler for both states components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "16d607587ca67d7b"; }
+/******/ 		__webpack_require__.h = function() { return "96c876cccb7fc811"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
